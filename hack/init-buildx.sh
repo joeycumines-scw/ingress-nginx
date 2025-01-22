fc80eeb5614ca7f1f189ef52f2dc1036ccc14ad5
Joseph Cumines <jcumines@securecodewarrior.com>
2025-01-22 14:26:54 +1000

    Fix setup binfmt_misc failing for remote buildx

--- hack/init-buildx.sh
+++ hack/init-buildx.sh
@@ -29,21 +29,32 @@ if ! docker buildx >/dev/null 2>&1; then
   exit 1
 fi
 
+if ! current_builder="$(docker buildx inspect)"; then
+  echo "ERROR: docker buildx inspect failed: exit code $?" >&2
+  exit 1
+fi
+
 # Ensure qemu is in binfmt_misc
 # Docker desktop already has these in versions recent enough to have buildx
 # We only need to do this setup on linux hosts
+# Any error is treated as a warning if the buildx driver is "remote"
 if [ "$(uname)" == 'Linux' ]; then
   # NOTE: this is pinned to a digest for a reason!
   # Note2 (@rikatz) - Removing the pin, as apparently it's breaking new alpine builds
   # docker run --rm --privileged multiarch/qemu-user-static@sha256:28ebe2e48220ae8fd5d04bb2c847293b24d7fbfad84f0b970246e0a4efd48ad6 --reset -p yes
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  if ! docker run --rm --privileged multiarch/qemu-user-static --reset -p yes; then
+    if grep -q "^Driver: *remote$" <<<"${current_builder}"; then
+      echo "WARNING: binfmt_misc setup failed, cross-platform builds may not work" >&2
+    else
+      echo "ERROR: binfmt_misc setup failed, cross-platform builds will not work" >&2
+      exit 1
+    fi
+  fi
 fi
 
 # We can skip setup if the current builder has sufficient platforms (specified
 # as arguments) AND if it isn't the docker driver, which doesn't work
-if [ "$#" -gt 0 ] &&
-  current_builder="$(docker buildx inspect)" &&
-  ! grep -q "^Driver: *docker$" <<<"${current_builder}"; then
+if [ "$#" -gt 0 ] && ! grep -q "^Driver: *docker$" <<<"${current_builder}"; then
   ok=1
   for platform in "$@"; do
     if ! { grep '^Platforms:' <<<"${current_builder}" | grep -q "$platform"; }; then
